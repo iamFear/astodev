5ecd5dc6aa28cee1508dd2f5a6ce95a6097a95c0
Daniel Burgoa <danielburgoa50@gmail.com>
2026-09-21 16:58:42 -0500

    Lighthouse test improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://astodev.netlify.app',
   compressHTML: true,
+  build: {
+    inlineStylesheets: 'always',
+  },
 });
