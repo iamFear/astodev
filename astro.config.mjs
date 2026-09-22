237e6b8e5ef5180c6443f390a26a18e96cebb60c
Daniel Burgoa <danielburgoa50@gmail.com>
2026-09-22 13:59:26 -0500

    technical SEO and company structure added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://astodev.netlify.app',
+  site: 'https://astodev.com',
+  trailingSlash: 'always',
   compressHTML: true,
   build: {
     inlineStylesheets: 'always',
